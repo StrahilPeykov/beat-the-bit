58dadb3a21560bfc4d5464c168b7c40fb86a692f
Konstantin <k.i.stoyanov@student.tue.nl>
2025-01-03 16:36:36 +0200

    establishing basic functionality

--- My project/Assets/Scripts/GameFlowManager.cs
+++ My project/Assets/Scripts/GameFlowManager.cs
@@ -12,6 +12,7 @@ public class GameFlowManager : MonoBehaviour
     public Button submitButton;
     public TextMeshProUGUI feedbackText;
 
+
     [Header("Game Messages")]
     public string welcomeMessage = "Welcome to the Cipher Challenge!";
     public string instructions = "Instructions:\n- Decrypt the cipher displayed on the screen.\n- Enter your guess in the input box.\n- Use the 'Reveal Algorithm' button for help if needed.";
@@ -19,47 +20,68 @@ public class GameFlowManager : MonoBehaviour
     private string correctDecryption = "designforgamesbulgariaandromania";
 
     void Start()
-    {
-        InitializeWelcomeScreen();
-    }
-
-    void InitializeWelcomeScreen()
-    {
-        welcomeText.text = welcomeMessage + "\n\n" + instructions;
-        welcomeText.gameObject.SetActive(true);
-        cipherText.gameObject.SetActive(false);
-        inputField.gameObject.SetActive(false);
-        submitButton.gameObject.SetActive(false);
-        feedbackText.gameObject.SetActive(false);
-        nextButton.gameObject.SetActive(true);
-        nextButton.onClick.AddListener(ShowCipherScreen);
-    }
-
-    void ShowCipherScreen()
-    {
-        welcomeText.gameObject.SetActive(false);
-        cipherText.text = cipherMessage;
-        cipherText.gameObject.SetActive(true);
-        inputField.gameObject.SetActive(true);
-        submitButton.gameObject.SetActive(true);
-        feedbackText.gameObject.SetActive(true);
-        feedbackText.text = "";
-        nextButton.gameObject.SetActive(false);
-        submitButton.onClick.AddListener(CheckDecryption);
-    }
-
-    void CheckDecryption()
-    {
-        string playerGuess = inputField.text;
-        if (playerGuess.ToUpper() == correctDecryption.ToUpper())
         {
-            feedbackText.text = "Correct! You've decrypted the message!";
-            feedbackText.color = Color.green;
+            InitializeWelcomeScreen();
         }
-        else
+
+        void InitializeWelcomeScreen()
         {
-            feedbackText.text = "Incorrect. Try again!";
-            feedbackText.color = Color.red;
+            welcomeText.text = welcomeMessage + "\n\n" + instructions;
+            welcomeText.gameObject.SetActive(true);
+            cipherText.gameObject.SetActive(false);
+            inputField.gameObject.SetActive(false);
+            submitButton.gameObject.SetActive(false);
+            feedbackText.gameObject.SetActive(false);
+            nextButton.gameObject.SetActive(true);
+
+            SetButtonText(nextButton, "Next");
+            SetButtonText(submitButton, "Submit");
+
+            nextButton.onClick.AddListener(ShowCipherScreen);
         }
-    }
+
+        void ShowCipherScreen()
+        {
+            welcomeText.gameObject.SetActive(false);
+            cipherText.text = cipherMessage;
+            cipherText.gameObject.SetActive(true);
+            inputField.gameObject.SetActive(true);
+            submitButton.gameObject.SetActive(true);
+            feedbackText.gameObject.SetActive(true);
+            feedbackText.text = "";
+            nextButton.gameObject.SetActive(false);
+            submitButton.onClick.AddListener(CheckDecryption);
+        }
+        private void SetButtonText(Button button, string text)
+        {
+            TextMeshProUGUI buttonText = button.GetComponentInChildren<TextMeshProUGUI>();
+            if (buttonText != null)
+            {
+                buttonText.text = text;
+            }
+        }
+
+        void CheckDecryption()
+        {
+            string playerGuess = inputField.text;
+
+            if (string.IsNullOrEmpty(playerGuess))
+            {
+                feedbackText.text = "Please enter a guess.";
+                feedbackText.color = Color.yellow;
+                return;
+            }
+
+            if (playerGuess == correctDecryption)
+            {
+                feedbackText.text = "Correct! You've decrypted the message!";
+                feedbackText.color = Color.green;
+            }
+            else
+            {
+                feedbackText.text = "Incorrect. Try again!";
+                feedbackText.color = Color.red;
+                inputField.text = "";
+            }
+        }
 }
